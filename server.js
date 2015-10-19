a7e301ce65121089578a2c2ed48584b967e4e147
Royce Leung <royceleung@gmail.com>
2015-10-19 14:09:00 -0700

    Displays on local host. Added google API

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var express = require("express");
-var http = require("http");
 var app = express();
 var port = 3000;
 var ip = "127.0.0.1";
 
-var server = http.createServer(app);
-console.log("Listening");
-server.listen(port, ip);
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', function(req, res) {
+	res.redirect('/index.html');
+})
+
+app.post('/video', function(req, res) {
+	res.send('you did it');
+})
+
+
+app.listen(port);
+
+console.log('Server now listening on port ' + port);
